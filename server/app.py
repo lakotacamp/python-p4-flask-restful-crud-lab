94c1974f5a6c60ead8133516e37d933e73edb012
lakota <lakotajcamp@gmail.com>
2024-01-10 07:53:37 -0500

    lab complete

--- server/app.py
+++ server/app.py
@@ -16,7 +16,11 @@ db.init_app(app)
 
 api = Api(app)
 
-
+# - Making a PATCH request to this route with an object in the body should update one
+#   plant, and return the updated plant in the response
+# - Making a DELETE request to this route should delete one plant from the database.
+#   You should return a response of an empty string with the status code 204 (No Content)
+#   to indicate a successful deletion.
 class Plants(Resource):
 
     def get(self):
@@ -47,6 +51,21 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self,id):
+        record = Plant.query.filter_by(id=id).first()
+        data = request.get_json()
+        for attr in data:
+            setattr(record, attr, data[attr])
+        db.session.add(record)
+        db.session.commit()
+        return make_response(record.to_dict(),200)
+    
+    def delete(self,id):
+        record = Plant.query.filter_by(id=id).first()
+        db.session.add(record)
+        db.session.commit()
+        response_dict = {"message":"plant deleted"}
+        return make_response(response_dict, 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
